df97c2399da43aec57f926af5c9a47b5406f6923
labibmon <labibmon@gmail.com>
2022-12-01 03:02:41 +0700

    Setup PWA

--- next.config.js
+++ next.config.js
@@ -1,43 +1,29 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-// module.exports = nextConfig
+module.exports = withPWA({
 
-module.exports = () => {
-
-  const serverRuntimeConfig = {
+  serverRuntimeConfig: {
     rowSelectedAPI: {
       list: 'id, name, arrival, message, time, pax'
     },
     guestUrlAPI: {
       list: 'guest'
     }
-  }
-
-	const publicRuntimeConfig = {
-		baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-		apiUrl: process.env.NEXT_PUBLIC_API_URL,
-  }
-
-  return {
-    serverRuntimeConfig,
-    publicRuntimeConfig
-  }
-}
-
-module.exports = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.vercel.com',
-        port: '',
-        pathname: '/image/upload/**',
-      },
-    ],
   },
-}
+
+  publicRuntimeConfig: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  }, 
+
+  headers: {
+    "X-Frame-Options": "sameorigin",
+  },
+
+  // return {
+  //   serverRuntimeConfig,
+  //   publicRuntimeConfig
+  // }
+});
